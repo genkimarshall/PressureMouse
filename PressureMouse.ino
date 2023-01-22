bd6fd0dae8a22f0f76e92b00778c6ab5db5adbb9
Genki Marshall <genki@fastmail.com>
2023-01-22 00:12:43 -0800

    Add regular-mouse endpoint

--- PressureMouse.ino
+++ PressureMouse.ino
@@ -1,4 +1,4 @@
-// Note: PressureMouse class implicitly included via setting Tools -> USB Type -> Pressure Mouse in the Arduino IDE.
+// Note: PressureMouse,Mouse class implicitly included via setting Tools -> USB Type -> Pressure Mouse in the Arduino IDE.
 
 #include <USBHost_t36.h>
 
@@ -15,6 +15,9 @@ void setup() {
 void loop() {
   usbHost.Task();
   if (usbHostedMouse.available()) {
+    Mouse.move(usbHostedMouse.getMouseX(), usbHostedMouse.getMouseY(), usbHostedMouse.getWheel());
+    uint8_t buttons = usbHostedMouse.getButtons();
+    Mouse.set_buttons(buttons & 1, (buttons >> 2) & 1, (buttons >> 1) & 1);
     PressureMouse.transmit(usbHostedMouse.getMouseX(), usbHostedMouse.getMouseY(), analogRead(FSR_PIN));
     usbHostedMouse.mouseDataClear();
   }
